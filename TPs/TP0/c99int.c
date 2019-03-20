2a3666165162632c757615ecdbfc4bf4d0b23747
Sebastian <sebastiandripari@gmail.com>
2019-03-20 01:36:15 -0300

    fixed untracked files

---
+++ TPs/TP0/c99int.c
@@ -0,0 +1,24 @@
+typedef char int8_t;
+typedef unsigned char uint8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef long int64_t;
+typedef unsigned long uint64_t;
+
+#include <stdio.h>
+
+int main() {
+	/*
+	printf("int8_t = %i\n", sizeof(int8_t) * 8);
+	printf("uint8_t = %i\n", sizeof(uint8_t) * 8);
+	printf("int16_t = %i\n", sizeof(int16_t) * 8);
+	printf("uint16_t = %i\n", sizeof(uint16_t) * 8);
+	printf("int32_t = %i\n", sizeof(int32_t) * 8);
+	printf("uint32_t = %i\n", sizeof(uint32_t) * 8);
+	printf("int64_t = %i\n", sizeof(int64_t) * 8);
+	printf("uint64_t = %i\n", sizeof(uint64_t) * 8);
+	*/
+	return 0;
+}
