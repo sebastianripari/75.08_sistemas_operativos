a44d0bb9fa9e484b9f7510f98369bbfe488790af
Sebastian Ripari <sebastiandripari@gmail.com>
2019-03-21 19:28:11 -0300

    Ahora imprime una palabra.

--- TPs/TP0/kern0.c
+++ TPs/TP0/kern0.c
@@ -2,22 +2,21 @@
 
 #include "c99int.h"
 
+
 static void
 vga_write(const char *s, int8_t linea, uint8_t color) {
 	volatile char *buf = VGABUF;
-	buf += 160*linea;
-	
-	*buf++ = 79;
-	*buf++ = 47;
-	*buf++ = 75;
-	*buf++ = 47;
+	uint8_t size_line = 160;
+	buf += size_line * linea;
+	for(int8_t i = 0; i < sizeof(s); ++i) {
+		*buf++ = (uint8_t) s[i];
+		*buf++ = color;	
+	}
 }
 
 void comienzo(void) {
-    
- 
-    const char s = "Hola";
-    vga_write(&s, 4, 47);
+	char s[] = "hola";
+    vga_write(s, 4, 47);
 
     while (1)
         asm("hlt");
