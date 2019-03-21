0379092b99d378a753e5169aa17fb82491567966
Sebastian Ripari <sebastiandripari@gmail.com>
2019-03-21 19:16:50 -0300

    vga_write

--- TPs/TP0/kern0.c
+++ TPs/TP0/kern0.c
@@ -1,4 +1,24 @@
+#define VGABUF ((volatile char *) 0xb8000)
+
+#include "c99int.h"
+
+static void
+vga_write(const char *s, int8_t linea, uint8_t color) {
+	volatile char *buf = VGABUF;
+	buf += 160*linea;
+	
+	*buf++ = 79;
+	*buf++ = 47;
+	*buf++ = 75;
+	*buf++ = 47;
+}
+
 void comienzo(void) {
+    
+ 
+    const char s = "Hola";
+    vga_write(&s, 4, 47);
+
     while (1)
-        continue;
+        asm("hlt");
 }
