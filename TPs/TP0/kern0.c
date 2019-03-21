b4b06a037a55045d28a7e405d5edb940f420fc9d
Sebastian Ripari <sebastiandripari@gmail.com>
2019-03-21 19:33:26 -0300

    Termino EJ lineas pantalla carc

--- TPs/TP0/kern0.c
+++ TPs/TP0/kern0.c
@@ -2,12 +2,18 @@
 
 #include "c99int.h"
 
-
 static void
 vga_write(const char *s, int8_t linea, uint8_t color) {
 	volatile char *buf = VGABUF;
-	uint8_t size_line = 160;
-	buf += size_line * linea;
+	uint8_t cantidad_caracteres_linea = 160;
+	uint8_t cantidad_lineas = 24;
+
+	if(linea >= 0) 
+		buf += cantidad_caracteres_linea * linea;
+	else
+		buf += cantidad_caracteres_linea * (cantidad_lineas + linea); 
+
+	
 	for(int8_t i = 0; i < sizeof(s); ++i) {
 		*buf++ = (uint8_t) s[i];
 		*buf++ = color;	
@@ -16,8 +22,7 @@ vga_write(const char *s, int8_t linea, uint8_t color) {
 
 void comienzo(void) {
 	char s[] = "hola";
-    vga_write(s, 4, 47);
+    vga_write(s, -3, 47);
 
-    while (1)
-        asm("hlt");
+    while(1) asm("hlt");
 }
